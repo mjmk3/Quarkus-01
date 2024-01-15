93603ee37a08663ea0d1f06004fdf237d6699690
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-15 05:30:28 +0300

    Quarkus v0.1

---
+++ src/main/java/org/acme/Model/User.java
@@ -0,0 +1,65 @@
+package org.acme.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * @author MJ Makki
+ * @version 1.0
+ * @license SkyLimits, LLC (<a href="https://www.skylimits.tech">SkyLimits, LLC</a>)
+ * @email m.makki@skylimits.tech
+ * @since long time ago
+ */
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String phone;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String phone, String username, String email) {
+        this.phone = phone;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

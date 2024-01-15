cf23d380bbf5749cf4b3b73838589df042a0024c
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-15 05:52:26 +0300

    Quarkus v0.1

--- src/main/java/org/acme/Controller/UserController.java
+++ src/main/java/org/acme/Controller/UserController.java
@@ -1,11 +1,9 @@
 package org.acme.Controller;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.acme.Model.User;
 import org.acme.Repository.UserRepository;
 
@@ -30,5 +28,43 @@ public class UserController {
     @GET
     public List<User> getAllUsers() {
         return userRepository.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public User getUserById(@PathParam("id") Long id) {
+        return userRepository.findById(id);
+    }
+
+    @POST
+    public Response createUser(User user) {
+        userRepository.persist(user);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateUser(@PathParam("id") Long id, User user) {
+        User existingUser = userRepository.findById(id);
+        if (existingUser != null) {
+            userRepository.getEntityManager().merge(user);
+            return Response.ok().build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteUser(@PathParam("id") Long id)
+
+    {
+        User user = userRepository.findById(id);
+        if (user != null) {
+            userRepository.deleteById(id);
+            return Response.ok().build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
